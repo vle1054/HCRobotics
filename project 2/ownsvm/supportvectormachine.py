4b8df505ca650aea7366dc5d69a4db9112941d5d
vle1054 <vle1054@gmail.com>
2018-03-18 16:42:21 -0600

    changes to svm

--- project 2/ownsvm/supportvectormachine.py
+++ project 2/ownsvm/supportvectormachine.py
@@ -1,21 +1,63 @@
-import os, string, math
+#!/usr/bin/env python
+import os, string, math, sys
 import numpy as np
 from svm import *
 from svmutil import *
-
-path = os.getcwd().replace ('\\','/')
+from subprocess import *
+from grid import *
 
 y, x = svm_read_problem('../heart_scale')
 m = svm_train(y[:200], x[:200], '-c 4')
 p_label, p_acc, p_val = svm_predict(y[200:], x[200:], m)
 
 
+if len(sys.argv) <= 1:
+	print('Usage: {0} training_file [testing_file]'.format(sys.argv[0]))
+	raise SystemExit
+
+# svm, grid, and gnuplot executable files
+
+is_win32 = (sys.platform == 'win32')
+if not is_win32:
+	svmscale_exe = "./svm-scale"
+	grid_py = "./grid.py"
+	gnuplot_exe = "/usr/bin/gnuplot"
+else:
+        # example for windows
+	svmscale_exe = r"..\windows\svm-scale.exe"
+	gnuplot_exe = r"c:\tmp\gnuplot\binary\pgnuplot.exe"
+	grid_py = r".\grid.py"
+
+assert os.path.exists(svmscale_exe),"svm-scale executable not found"
+assert os.path.exists(gnuplot_exe),"gnuplot executable not found"
+assert os.path.exists(grid_py),"grid.py not found"
+
+train_pathname = sys.argv[1]
+assert os.path.exists(train_pathname),"training file not found"
+file_name = os.path.split(train_pathname)[1]
+scaled_file = file_name + ".scale"
+model_file = file_name + ".model"
+range_file = file_name + ".range"
+
+if len(sys.argv) > 2:
+	test_pathname = sys.argv[2]
+	file_name = os.path.split(test_pathname)[1]
+	assert os.path.exists(test_pathname),"testing file not found"
+	scaled_test_file = file_name + ".scale"
+	predict_test_file = file_name + ".predict"
+
+cmd = '{0} -s "{1}" "{2}" > "{3}"'.format(svmscale_exe, range_file, train_pathname, scaled_file)
+print('Scaling training data...')
+Popen(cmd, shell = True, stdout = PIPE).communicate()	
+
+ rate, param = find_parameters(dataset, options)
 
 
+print('Cross validation...')
 
+rate, param = find_parameters('../heart_scale',"")
 
-
-
+print(param)
 
 
 
